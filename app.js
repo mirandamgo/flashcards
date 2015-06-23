e8efae5f1ff735c3a57fec1ef0612de309021275
mirandamgo <mirandago98@gmail.com>
2015-06-23 14:27:45 -0700

    basic jquery code

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-var data = {
-	labels: ["incorrect", "correct"],
-	datasets: [
-		{
-			data: [40,60]
-		}
-	]
-};
-var ctx = document.getElementById("myChart").getContext("2d");
-var myNewChart = new Chart(ctx).Bar(data);
+$(function() {
+	var data = {
+		labels: ["incorrect", "correct"],
+		datasets: [
+			{
+				data: [40,60]
+			}
+		]
+	};
+	var ctx = document.getElementById("myChart").getContext("2d");
+	var myNewChart = new Chart(ctx).Bar(data);
+	
+	$(".flashcard-box").on("click", function() {
+		$(".flashcard-box").html("hello");
+	});
+});
