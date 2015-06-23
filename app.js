03e3716c9f4a5a5df47585cb60afc765b91df781
mirandamgo <mirandago98@gmail.com>
2015-06-23 13:18:24 -0700

    add example chart

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello");
+var data = {
+	labels: ["incorrect", "correct"],
+	datasets: [
+		{
+			data: [40,60]
+		}
+	]
+};
+var ctx = document.getElementById("myChart").getContext("2d");
+var myNewChart = new Chart(ctx).Bar(data);
